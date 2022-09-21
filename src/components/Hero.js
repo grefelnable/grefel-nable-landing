b2908128c7e73bb2c6b65dbdccd47647513654b8
Grefel Nable <felnable@gmail.com>
2022-09-21 13:30:48 -0400

    Mask image using daisyui

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,28 +1,68 @@
 import React from "react";
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaPaperPlane,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+import GrefelHoodie from "../assets/grefel-hoodie-remove-bg.png";
 
 const Hero = () => {
   return (
-    <header className="hero min-h-screen bg-base-200">
-      <div className="section-center">
-        <div className="hero-content flex-col justify-between lg:flex-row-reverse">
-          <img
-            src="https://placeimg.com/260/400/arch"
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h3>I'm</h3>
-            <h1 className="text-[9rem] font-bold">GREFEL</h1>
-            <h2>Frontend Developer</h2>
-            <a
-              href="mailto: felnable@gmail.com"
-              class="bg-violet-500 text-white px-5 py-3 rounded-xl mb-20 tracking-wide shadow-md border-2 border-violet-500 ease-in-out duration-300 hover:shadow-2xl hover:bg-transparent hover:text-slate-900 "
-            >
-              Let's talk{" "}
-              <span class="ml-2">
-                <i class="fa-solid fa-paper-plane"></i>
-              </span>
-            </a>
-          </div>
+    <header className="hero h-screen bg-base-200">
+      <div className="section-center flex flex-col justify-between lg:flex-row-reverse ">
+        <div className="">
+          <img src={GrefelHoodie} className="object-cover mask mask-hexagon" />
+        </div>
+
+        <div>
+          <h3>I'm</h3>
+          <h1 className="text-[9rem] font-bold">GREFEL</h1>
+          <h2 className="mb-8">Frontend Developer</h2>
+          <a
+            href="mailto: felnable@gmail.com"
+            class="bg-violet-500 text-white px-5 py-3 rounded-xl mb-20 tracking-wide shadow-md border-2 border-violet-500 ease-in-out duration-300 hover:shadow-2xl hover:bg-transparent hover:text-slate-900 "
+          >
+            Let's talk
+            <FaPaperPlane className="inline ml-4" />
+          </a>
+          <p className="capitalize mb-4 tracking-wider">Check out my</p>
+          {/* hero social icons */}
+          <ul className="flex gap-6 text-slate-900 text-2xl">
+            <li className="btn-social">
+              <a
+                href="https://www.linkedin.com/in/grefel-nable/"
+                target="_blank"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+            <li className="btn-social">
+              <a href="https://github.com/grefelnable" target="_blank">
+                <FaGithub />
+              </a>
+            </li>
+            <li className="btn-social">
+              <a
+                href="https://www.youtube.com/channel/UCkY2ed8qf0OlLjEjWXsVJbw"
+                target="_blank"
+              >
+                <FaYoutube />
+              </a>
+            </li>
+            <li className="btn-social">
+              <a href="https://twitter.com/grefel_nable" target="_blank">
+                <FaTwitter />
+              </a>
+            </li>
+            <li className="btn-social">
+              <a href="https://www.instagram.com/grefelcodes/" target="_blank">
+                <FaInstagram />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
