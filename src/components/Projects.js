6abdec1138174438633da2557c38694e5209a27d
Grefel Nable <felnable@gmail.com>
2022-09-22 10:21:13 -0400

    Transfer workout snapshot under public
    Also added the img path for the project image. deconstruct the project array of objects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { projectsFile } from "../projects-data";
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsFile);
+
+  React.useEffect(() => {
+    console.log(projects);
+  }, []);
   return (
     <section
       id="projects"
       className="h-screen pt-12 bg-[url(https://placeimg.com/640/480/tech)] bg-no-repeat bg-cover"
     >
       <div className="section-center">
-        <h2>Projects</h2>
+        <h2 className="text-primary mb-6">Projects</h2>
+        {/* project cards */}
+        <div>
+          {projects.map((project) => {
+            const { id, name, img, description, preview, code } = project;
+            return (
+              <article key={id}>
+                <div className="card w-96 glass">
+                  <figure>{/* <img src= alt="car!" /> */}</figure>
+                  <div className="card-body">
+                    <h2 className="card-title">Life hack</h2>
+                    <p>How to park your car at your garage?</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Learn now!</button>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
---
+++ src/projects-data.js
@@ -0,0 +1,27 @@
+export const projectsFile = [
+  {
+    id: 1,
+    name: "workout log",
+    img: "./project-snapshots/workout-log-snapshot.png",
+    description:
+      "Workout tracking app. User can name the workout like biceps curl. With that workout user can also log the repetition and how many sets done.",
+    preview: "https://workout-log-7c88b.firebaseapp.com/",
+    code: "https://github.com/grefelnable/workout-log",
+  },
+  {
+    id: 2,
+    name: "workout log",
+    description:
+      "Workout tracking app. User can name the workout like biceps curl. With that workout user can also log the repetition and how many sets done.",
+    preview: "https://workout-log-7c88b.firebaseapp.com/",
+    code: "https://github.com/grefelnable/workout-log",
+  },
+  {
+    id: 3,
+    name: "workout log",
+    description:
+      "Workout tracking app. User can name the workout like biceps curl. With that workout user can also log the repetition and how many sets done.",
+    preview: "https://workout-log-7c88b.firebaseapp.com/",
+    code: "https://github.com/grefelnable/workout-log",
+  },
+];
