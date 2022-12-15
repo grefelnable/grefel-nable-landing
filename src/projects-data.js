e3fea341263db08e771f9cc711fa4e7a7a9539e8
Grefel Nable <felnable@gmail.com>
2022-12-15 02:02:33 -0500

    Add latest phone book app in projects

--- src/projects-data.js
+++ src/projects-data.js
@@ -1,6 +1,15 @@
 export const projectsFile = [
   {
     id: 1,
+    name: "phone book",
+    img: "./project-snapshots/phonebook-snapshot.png",
+    description:
+      "Phone book app that let you store important person's mobile number. It has basic features that includes adding, editing and deleting a contact.",
+    preview: "https://phonebook-grefel.web.app/",
+    code: "https://github.com/grefelnable/phonebook-grefel",
+  },
+  {
+    id: 2,
     name: "workout log",
     img: "./project-snapshots/workout-snapshot.png",
     description:
@@ -9,7 +18,7 @@ export const projectsFile = [
     code: "https://github.com/grefelnable/workout-log",
   },
   {
-    id: 2,
+    id: 3,
     name: "grefel's landing page",
     img: "./project-snapshots/grefel-snapshot.png",
     description: "My personal landing page.",
